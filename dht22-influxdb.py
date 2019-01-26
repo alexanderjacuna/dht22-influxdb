f03e5fee5ae14a2a87acaf1487f6e31bf8eb1114
Alexander Acuña <38297159+alexanderjacuna@users.noreply.github.com>
2019-01-26 00:33:32 -0600

    Initial upload of script.

---
+++ dht22-influxdb.py
@@ -0,0 +1,53 @@
+import sys
+import datetime
+import Adafruit_DHT
+from influxdb import InfluxDBClient
+
+#SET VARUABLES
+host = "192.168.1.67"
+port = 8086
+user = "user"
+password = "password" 
+dbname = "readings"
+
+#CREATE CLIENT OBJECT
+client = InfluxDBClient(host, port, user, password, dbname)
+
+#SENSOR DETAILS
+sensor = Adafruit_DHT.DHT22
+sensor_gpio = 17
+
+#SET MEASUREMENT VARUABLES
+measurement1 = "venus-dht22-temperature"
+measurement2 = "venus-dht22-humidity"
+
+#GET VARUABLES
+humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_gpio)
+
+temperature =  round(temperature * 9/5.0 + 32,2)
+humidity = round(humidity,1)
+
+# Print for debugging, uncomment the below line
+print("Temp: %s, Humidity: %s" % (temperature, humidity)) 
+
+data1 = [
+{
+  "measurement": measurement1,
+	  "fields": {
+		  "temperature" : temperature
+	  }
+  } 
+]
+
+data2 = [
+{
+  "measurement": measurement2,
+	  "fields": {
+		  "humidity" : humidity
+	  }
+  } 
+]
+
+#WRITE DATA
+client.write_points(data1)
+client.write_points(data2)
